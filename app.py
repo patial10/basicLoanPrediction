9f170edfb92f5c088c8b25ab7f38b12b355a6645
patial10 <75527076+patial10@users.noreply.github.com>
2020-12-29 23:10:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+
+app = Flask(__name__)
+model=pickle.load(open("savedFile.pkl","rb"))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+#     output = round(prediction[0], 2)
+    if prediction == 'N':
+        prediction="This Person is not able to take the loan on the basis of his/her informaton"
+    else:
+        prediction="This person is able to take the loan on the basis of his/her informaton"
+        
+    return render_template('home.html', prediction_text=' {} '.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
